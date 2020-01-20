27b13e3b05dc7f1f465ff09d21663fa3d3a22a3e
KNHui <37471802+KNHui@users.noreply.github.com>
2020-01-20 23:05:52 +0900

    feat: Update model of room and review
    Add functions to get review rating

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from . import models
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
-    
+
     """ Custom User Admin """
 
     fieldsets = UserAdmin.fieldsets + (
@@ -23,4 +23,19 @@ class CustomUserAdmin(UserAdmin):
                 )
             },
         ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
     )
